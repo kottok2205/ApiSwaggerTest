d240336ab4ed3b0088321a6f6048151468b3623b
kottok2205 <tarasyuk222001@gmail.com>
2024-03-13 19:47:26 +0200

    Обновить .dtbcache.v2 и 4c9c58e0-85ce-49a8-8a89-a469cd033189.vsidx и ещё 10 файла...

--- ApiSwaggerTest/GetPetIdTest.cs
+++ ApiSwaggerTest/GetPetIdTest.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework.Legacy;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace ApiSwaggerTest
 {
@@ -10,12 +11,17 @@ namespace ApiSwaggerTest
         public async Task PostAPITest(string category)
         {
             var requestJson = File.ReadAllText(@"d:\VSProject\APISwaggerTest\ApiSwaggerTest\request.json");
-            var content = new StringContent(requestJson, Encoding.UTF8, "application/json");
-            var response = await Client.PostAsync(baseUrl + $"{category}", content);
+            var responseJson = File.ReadAllText(@"d:\VSProject\APISwaggerTest\ApiSwaggerTest\response.json");
+            var requestContent = new StringContent(requestJson, Encoding.UTF8, "application/json");
+            var response = await Client.PostAsync(baseUrl + $"{category}", requestContent);
 
             ClassicAssert.IsTrue(response.IsSuccessStatusCode);
 
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var normalizedExpected = Regex.Replace(responseJson, @"\s+", "");
+            var normalizedActual = Regex.Replace(responseContent, @"\s+", "");
 
+            ClassicAssert.AreEqual(normalizedExpected, normalizedActual);
         }
 
         [TestCase("pet", "123456789")]
@@ -23,12 +29,16 @@ namespace ApiSwaggerTest
         public async Task GetAPITest(string category, string idCategory)
         {
             HttpResponseMessage response = await Client.GetAsync(baseUrl + $"{category}/{idCategory}");
+            var responseJson = File.ReadAllText(@"d:\VSProject\APISwaggerTest\ApiSwaggerTest\response.json");
 
             ClassicAssert.IsTrue(response.IsSuccessStatusCode);
 
             string responseData = await response.Content.ReadAsStringAsync();
 
-            ClassicAssert.IsNotNull(responseData);
+            var normalizedExpected = Regex.Replace(responseJson, @"\s+", "");
+            var normalizedActual = Regex.Replace(responseData, @"\s+", "");
+
+            ClassicAssert.AreEqual(normalizedExpected, normalizedActual);
         }
     }
 }
